eacb130efae1b6f17cb623f76f2485d21ac47df0
Patrick Lioi <patrick.lioi@gmail.com>
2014-02-06 21:16:04 -0600

    Update Fixie to latest version.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Parsley")]
 [assembly: AssemblyVersion("0.0.5.0")]
 [assembly: AssemblyFileVersion("0.0.5.0")]
-[assembly: AssemblyCopyright("Copyright (c) 2011-2013 Patrick Lioi")]
+[assembly: AssemblyCopyright("Copyright (c) 2011-2014 Patrick Lioi")]
 [assembly: AssemblyCompany("Patrick Lioi")]
 [assembly: AssemblyDescription("A monadic parser combinator library inspired by Haskell's Parsec library.")]
 [assembly: AssemblyConfiguration("Release")]
