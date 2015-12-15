daa453dded84c5bd0cab00e096ed4f6738428e36
Patrick Lioi <patrick.lioi@gmail.com>
2015-12-15 09:16:40 -0600

    The build script owns the contents of all AssemblyInfo.cs files, instead of using a shared CommonAssemblyInfo.cs file.

--- build.ps1
+++ build.ps1
@@ -3,13 +3,13 @@ Framework '4.5.2'
 properties {
     $birthYear = 2011
     $maintainers = "Patrick Lioi"
-    $description = "A monadic parser combinator library inspired by Haskell's Parsec library."
 
     $configuration = 'Release'
     $src = resolve-path '.\src'
     $tools = resolve-path '.\tools'
     $build = if ($env:build_number -ne $NULL) { $env:build_number } else { '0' }
     $version = [IO.File]::ReadAllText('.\VERSION.txt') + '.' + $build
+    $projects = @(gci $src -rec -filter *.csproj)
 }
 
 task default -depends Test
@@ -29,26 +29,65 @@ task Test -depends Compile {
     exec { & $fixieRunner $src\Parsley.Test\bin\$configuration\Parsley.Test.dll }
 }
 
-task Compile -depends CommonAssemblyInfo {
+task Compile -depends AssemblyInfo, License {
   exec { msbuild /t:clean /v:q /nologo /p:Configuration=$configuration $src\Parsley.sln }
   exec { msbuild /t:build /v:q /nologo /p:Configuration=$configuration $src\Parsley.sln }
 }
 
-task CommonAssemblyInfo {
-    $date = Get-Date
-    $year = $date.Year
-    $copyrightSpan = if ($year -eq $birthYear) { $year } else { "$birthYear-$year" }
-    $copyright = "Copyright (c) $copyrightSpan $maintainers"
+task AssemblyInfo {
+    $copyright = get-copyright
 
-"using System.Reflection;
+    foreach ($project in $projects) {
+        $projectName = [System.IO.Path]::GetFileNameWithoutExtension($project)
+
+        if ($projectName -eq "Build") {
+            continue;
+        }
+
+        regenerate-file "$($project.DirectoryName)\Properties\AssemblyInfo.cs" @"
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyProduct(""Parsley"")]
-[assembly: AssemblyVersion(""$version"")]
-[assembly: AssemblyFileVersion(""$version"")]
-[assembly: AssemblyCopyright(""$copyright"")]
-[assembly: AssemblyCompany(""$maintainers"")]
-[assembly: AssemblyDescription(""$description"")]
-[assembly: AssemblyConfiguration(""$configuration"")]" | out-file "$src\CommonAssemblyInfo.cs" -encoding "ASCII"
+[assembly: AssemblyProduct("Parsley")]
+[assembly: AssemblyTitle("$projectName")]
+[assembly: AssemblyVersion("$version")]
+[assembly: AssemblyFileVersion("$version")]
+[assembly: AssemblyCopyright("$copyright")]
+[assembly: AssemblyCompany("$maintainers")]
+[assembly: AssemblyConfiguration("$configuration")]
+"@
+    }
+}
+
+task License {
+    $copyright = get-copyright
+
+    regenerate-file "LICENSE.txt" @"
+The MIT License (MIT)
+$copyright
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"@
+}
+
+function get-copyright {
+    $date = Get-Date
+    $year = $date.Year
+    $copyrightSpan = if ($year -eq $birthYear) { $year } else { "$birthYear-$year" }
+    return "Copyright © $copyrightSpan $maintainers"
+}
+
+function regenerate-file($path, $newContent) {
+    $oldContent = [IO.File]::ReadAllText($path)
+
+    if ($newContent -ne $oldContent) {
+        $relativePath = Resolve-Path -Relative $path
+        write-host "Generating $relativePath"
+        [System.IO.File]::WriteAllText($path, $newContent, [System.Text.Encoding]::UTF8)
+    }
 }
--- src/Parsley/Properties/AssemblyInfo.cs
+++ src/Parsley/Properties/AssemblyInfo.cs
@@ -1,2 +1,11 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Parsley")]
 [assembly: AssemblyTitle("Parsley")]
+[assembly: AssemblyVersion("0.0.5.0")]
+[assembly: AssemblyFileVersion("0.0.5.0")]
+[assembly: AssemblyCopyright("Copyright © 2011-2015 Patrick Lioi")]
+[assembly: AssemblyCompany("Patrick Lioi")]
+[assembly: AssemblyConfiguration("Release")]
