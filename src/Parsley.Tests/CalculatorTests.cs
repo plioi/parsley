0a15921ca16b73129f8ea8dede14c4be2d0cdda9
Patrick Lioi <patrick.lioi@gmail.com>
2016-06-08 18:44:04 -0500

    Introduce a sample test class to help prove out xUnit .NET Core setup. After running tests within Visual Studio's Test Explorer for the first time, Visual Studio automatically marked the project as a test project by adding a Service tag within Parsley.Tests.xproj.

---
+++ src/Parsley.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Parsley.Tests
+{
+    using Xunit;
+
+    public class CalculatorTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        static int Add(int x, int y) => x + y;
+    }
+}
