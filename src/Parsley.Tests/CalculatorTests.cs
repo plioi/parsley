29a693c7602f9c8c509db065a5fe1f3975f7cebb
Patrick Lioi <patrick.lioi@gmail.com>
2016-06-08 19:05:17 -0500

    Prove that the Shouldly library has been successfully referenced, by rephrasing all sample test assertions to be Shouldly assertions.

--- src/Parsley.Tests/CalculatorTests.cs
+++ src/Parsley.Tests/CalculatorTests.cs
@@ -1,19 +1,20 @@
 ﻿namespace Parsley.Tests
 {
     using Xunit;
+    using Shouldly;
 
     public class CalculatorTests
     {
         [Fact]
         public void PassingTest()
         {
-            Assert.Equal(4, Calculator.Add(2, 2));
+            Calculator.Add(2, 2).ShouldBe(4);
         }
 
         [Fact]
         public void FailingTest()
         {
-            Assert.Equal(5, Calculator.Add(2, 2));
+            Calculator.Add(2, 2).ShouldBe(5);
         }
     }
 }
