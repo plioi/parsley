0cc23daa7e2cb5ae43d0c957b3ca6d3661abec7a
Patrick Lioi <patrick.lioi@gmail.com>
2016-06-14 07:46:55 -0500

    The build script can produce versioned NuGet packages when running under AppVeyor.

--- build.ps1
+++ build.ps1
@@ -2,6 +2,10 @@ function main {
     step { Restore }
     step { Build }
     step { Test }
+
+    if ($env:APPVEYOR_BUILD_NUMBER -ne $NULL) {
+        step { Pack }
+    }
 }
 
 function Restore {
@@ -15,6 +19,15 @@ function Build() {
 
 function Test {
     exec { & dotnet test .\src\Parsley.Tests --configuration Release }
+}
+
+function Pack {
+    if ([System.IO.Directory]::Exists(".\artifacts")) {
+        [System.IO.Directory]::Delete(".\artifacts", $true)
+    }
+    $revision = $env:APPVEYOR_BUILD_NUMBER
+    $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+    exec { & dotnet pack .\src\Parsley --configuration Release --output .\artifacts --version-suffix=$revision }
 }
 
 function step($block) {
