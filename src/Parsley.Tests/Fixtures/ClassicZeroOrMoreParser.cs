eb0ccfce192bfea784b8a4be5169e56104d60031
Dmitrii Nemtsov <asssvk@gmail.com>
2018-11-02 18:35:31 -0500

    Enhanced the QuantifiedParser unit tests.

---
+++ src/Parsley.Tests/Fixtures/ClassicZeroOrMoreParser.cs
@@ -0,0 +1,45 @@
+using Parsley.Parsers;
+using System;
+using System.Collections.Generic;
+
+namespace Parsley.Tests.Fixtures
+{
+    internal class ClassicZeroOrMoreParser<T> : Parser<IEnumerable<T>>
+    {
+        private readonly IParser<T> _item;
+
+        public ClassicZeroOrMoreParser(IParser<T> item)
+        {
+            _item = item;
+        }
+
+        public override IReply<IEnumerable<T>> Parse(TokenStream tokens)
+        {
+            var oldPosition = tokens.Position;
+            var reply = _item.Parse(tokens);
+            var newPosition = reply.UnparsedTokens.Position;
+
+            var list = new List<T>();
+
+            while (reply.Success)
+            {
+                if (oldPosition == newPosition)
+                    throw new Exception($"Parser encountered a potential infinite loop at position {newPosition}.");
+
+                list.Add(reply.Value);
+                oldPosition = newPosition;
+                reply = _item.Parse(reply.UnparsedTokens);
+                newPosition = reply.UnparsedTokens.Position;
+            }
+
+            //The item parser finally failed.
+
+            if (oldPosition != newPosition)
+                return new Error<IEnumerable<T>>(reply.UnparsedTokens, reply.ErrorMessages);
+
+            return new Parsed<IEnumerable<T>>(list, reply.UnparsedTokens, reply.ErrorMessages);
+        }
+
+        protected override string GetName() => "";
+    }
+}
