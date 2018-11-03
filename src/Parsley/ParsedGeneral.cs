8ea045b596e516aa8b7cc6061628430c8cb2259d
Dmitrii Nemtsov <asssvk@gmail.com>
2018-11-02 21:23:34 -0500

    General Parsing Optimization: Implemented IGeneralParser in MonadicUnitParser.

---
+++ src/Parsley/ParsedGeneral.cs
@@ -0,0 +1,15 @@
+﻿namespace Parsley
+{
+    public class ParsedGeneral : IGeneralReply
+    {
+        public ParsedGeneral(TokenStream unparsedTokens)
+        {
+            UnparsedTokens = unparsedTokens;
+        }
+        
+        public TokenStream UnparsedTokens { get; }
+        public bool Success => true;
+
+        public virtual ErrorMessageList ErrorMessages => ErrorMessageList.Empty;
+    }
+}
