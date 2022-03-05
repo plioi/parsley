5c7c19f7626a7a783f8f5dae156cc143c76159be
Patrick Lioi <patrick.lioi@gmail.com>
2022-03-05 11:47:02 -0600

    Because an in-place upgrade from .NET Core 1.0 is needlessly challenging, begin with a clean slate.

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dotnet restore --verbosity Warning src/Build
-dotnet run --project src/Build -- $*
--- src/Parsley/Primitives/FailingParser.cs
+++
@@ -1,8 +0,0 @@
-namespace Parsley.Primitives
-{
-    internal class FailingParser<T> : Parser<T>
-    {
-        public Reply<T> Parse(TokenStream tokens)
-            => new Error<T>(tokens, ErrorMessage.Unknown());
-    }
-}
