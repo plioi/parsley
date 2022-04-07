f5e3c8e12f3e6019a7e55c4475a7c715a7d9cf97
Patrick Lioi <patrick.lioi@gmail.com>
2022-04-07 08:26:32 -0500

    Position's value equality semantics now come from being a `record` type instead of inheriting from Value<T>, phasing out the need for Value<T>.

--- src/Parsley/Position.cs
+++ src/Parsley/Position.cs
@@ -1,19 +1,7 @@
 namespace Parsley;
 
-public class Position : Value<Position>
+public record Position(int Line, int Column)
 {
-    public int Line { get; }
-    public int Column { get; }
-
-    public Position(int line, int column)
-    {
-        Line = line;
-        Column = column;
-    }
-
-    protected override object[] ImmutableFields()
-        => new object[] {Line, Column};
-
-    public override string  ToString()
+    public override string ToString()
         => $"({Line}, {Column})";
 }
