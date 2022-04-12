8896768bd144ed840cbbc04cd10f0891f6466597
Patrick Lioi <patrick.lioi@gmail.com>
2022-04-11 23:46:16 -0500

    Organize Grammer member files.

---
+++ src/Parsley/Grammar.Optional.cs
@@ -0,0 +1,14 @@
+namespace Parsley;
+
+public static partial class Grammar
+{
+    /// <summary>
+    /// Optional(p) is equivalent to p whenever p succeeds or when p fails after consuming input.
+    /// If p fails without consuming input, Optional(p) succeeds.
+    /// </summary>
+    public static Parser<T> Optional<T>(Parser<T> parser)
+    {
+        var nothing = default(T).SucceedWithThisValue();
+        return Choice(parser, nothing);
+    }
+}
