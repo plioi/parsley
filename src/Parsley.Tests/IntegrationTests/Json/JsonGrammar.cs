999409acf78ddbc5d7b0b874d1acd15f7d8507b4
Patrick Lioi <patrick.lioi@gmail.com>
2022-04-11 21:25:29 -0500

    Simplify implementation of JsonGrammar for readability and to factor out redundant whitespace concerns.

--- src/Parsley.Tests/IntegrationTests/Json/JsonGrammar.cs
+++ src/Parsley.Tests/IntegrationTests/Json/JsonGrammar.cs
@@ -6,7 +6,7 @@ namespace Parsley.Tests.IntegrationTests.Json;
 
 public class JsonGrammar
 {
-    static readonly IParser<string> StringLiteral = Pattern("string", @"
+    static readonly IParser<string> Quote = Pattern("string", @"
             # Open quote:
             ""
 
@@ -14,14 +14,14 @@ public class JsonGrammar
             (
                       [^""\\]*             # Zero or more non-quote, non-slash characters.
                 |     \\ [""\\bfnrt\/]     # One of: slash-quote   \\   \b   \f   \n   \r   \t   \/
-                |     \\ u [0-9a-fA-F]{4}  # \u folowed by four hex digits
+                |     \\ u [0-9a-fA-F]{4}  # \u followed by four hex digits
             )*
 
             # Close quote:
             ""
         ");
 
-    static readonly IParser<string> NumberLiteral = Pattern("number", @"
+    static readonly IParser<string> Number = Pattern("number", @"
             # Look-ahead to confirm the whole-number part is either 0 or starts with 1-9:
             (?=
                 0(?!\d)  |  [1-9]
@@ -41,82 +41,65 @@ public class JsonGrammar
             )?
         ");
 
-    public static readonly IParser<object> Json;
-    static readonly IParser<object> JsonValue;
+    public static readonly IParser<object> JsonDocument;
+    static readonly IParser<object> Value;
 
     static JsonGrammar()
     {
         var Whitespace = Optional(Pattern("whitespace", @"\s+"));
 
-        var Comma = Symbol(",");
-        var OpenArray = Symbol("[");
-        var CloseArray = Symbol("]");
-        var OpenDictionary = Symbol("{");
-        var CloseDictionary = Symbol("}");
-        var Colon = Symbol(":");
-
-        var True =
-            from _ in Keyword("true")
+        var Key =
+            from leading in Whitespace
+            from quote in Quote
             from trailing in Whitespace
-            select (object)true;
-
-        var False =
-            from _ in Keyword("false")
-            from trailing in Whitespace
-            select (object)false;
-
-        var Null =
-            from _ in Keyword("null")
-            from trailing in Whitespace
-            select (object)null;
-
-        var Number =
-            from number in NumberLiteral
-            from trailing in Whitespace
-            select (object) decimal.Parse(number, NumberStyles.Any, CultureInfo.InvariantCulture);
-
-        var String =
-            from quotation in StringLiteral
-            from trailing in Whitespace
-            select Unescape(quotation);
-
-        var Array =
-            from open in OpenArray
-            from items in ZeroOrMore(JsonValue, Comma)
-            from close in CloseArray
-            select items.ToArray();
+            select Unquote(quote);
 
         var Pair =
-            from key in String
-            from colon in Colon
-            from value in JsonValue
+            from key in Key
+            from colon in Operator(":")
+            from value in Value
             select new KeyValuePair<string, object>(key, value);
 
-        var Dictionary =
-            from open in OpenDictionary
-            from pairs in ZeroOrMore(Pair, Comma)
-            from close in CloseDictionary
-            select pairs.ToDictionary(x => x.Key, x => x.Value);
-
-        JsonValue =
-            Choice(True, False, Null, Number, String, Dictionary, Array);
-
-        Json =
+        Value =
             from leading in Whitespace
-            from jsonValue in JsonValue
-            from end in EndOfInput
-            from trailing in Whitespace
-            select jsonValue;
+            from value in Choice(
+                from @true in Keyword("true")
+                select (object) true,
 
-        IParser<string> Symbol(string symbol) =>
-            from s in Operator(symbol)
+                from @false in Keyword("false")
+                select (object) false,
+
+                from @null in Keyword("null")
+                select (object) null,
+
+                from number in Number
+                select (object) decimal.Parse(number, NumberStyles.Any, CultureInfo.InvariantCulture),
+
+                from quotation in Quote
+                select Unquote(quotation),
+
+                from open in Operator("{")
+                from pairs in ZeroOrMore(Pair, Operator(","))
+                from close in Operator("}")
+                select pairs.ToDictionary(x => x.Key, x => x.Value),
+
+                from open in Operator("[")
+                from items in ZeroOrMore(Value, Operator(","))
+                from close in Operator("]")
+                select items.ToArray()
+            )
             from trailing in Whitespace
-            select s;
+            select value;
+
+        JsonDocument =
+            from value in Value
+            from end in EndOfInput
+            select value;
     }
 
-    static string Unescape(string quotation)
+    static string Unquote(string quote)
     {
-        string result = quotation.Substring(1, quotation.Length - 2); //Remove leading and trailing quotation marks
+        string result = quote.Substring(1, quote.Length - 2); //Remove leading and trailing quotation marks
 
         result = Regex.Replace(result, @"\\u[0-9a-fA-F]{4}",
             match => char.ConvertFromUtf32(int.Parse(match.Value.Replace("\\u", ""), NumberStyles.HexNumber, CultureInfo.InvariantCulture)));
--- src/Parsley.Tests/IntegrationTests/Json/JsonGrammarTests.cs
+++ src/Parsley.Tests/IntegrationTests/Json/JsonGrammarTests.cs
@@ -1,36 +1,38 @@
 namespace Parsley.Tests.IntegrationTests.Json;
 
-class JsonGrammarTests : JsonGrammar
+using static JsonGrammar;
+
+class JsonGrammarTests
 {
     public void ParsesTrueLiteral()
     {
-        Json.Parses("true").WithValue(true);
+        JsonDocument.Parses("true").WithValue(true);
     }
 
     public void ParsesFalseLiteral()
     {
-        Json.Parses("false").WithValue(false);
+        JsonDocument.Parses("false").WithValue(false);
     }
 
     public void ParsesNullLiteral()
     {
-        Json.Parses("null").WithValue((object)null);
+        JsonDocument.Parses("null").WithValue((object)null);
     }
 
     public void ParsesNumbers()
     {
-        Json.Parses("0").WithValue(0m);
-        Json.Parses("12345").WithValue(12345m);
-        Json.Parses("0.012").WithValue(0.012m);
-        Json.Parses("0e1").WithValue(0e1m);
-        Json.Parses("0e+1").WithValue(0e+1m);
-        Json.Parses("0e-1").WithValue(0e-1m);
-        Json.Parses("0E1").WithValue(0E1m);
-        Json.Parses("0E+1").WithValue(0E+1m);
-        Json.Parses("0E-1").WithValue(0E-1m);
-        Json.Parses("10e11").WithValue(10e11m);
-        Json.Parses("10.123e11").WithValue(10.123e11m);
-        Json.Parses("10.123E-11").WithValue(10.123E-11m);
+        JsonDocument.Parses("0").WithValue(0m);
+        JsonDocument.Parses("12345").WithValue(12345m);
+        JsonDocument.Parses("0.012").WithValue(0.012m);
+        JsonDocument.Parses("0e1").WithValue(0e1m);
+        JsonDocument.Parses("0e+1").WithValue(0e+1m);
+        JsonDocument.Parses("0e-1").WithValue(0e-1m);
+        JsonDocument.Parses("0E1").WithValue(0E1m);
+        JsonDocument.Parses("0E+1").WithValue(0E+1m);
+        JsonDocument.Parses("0E-1").WithValue(0E-1m);
+        JsonDocument.Parses("10e11").WithValue(10e11m);
+        JsonDocument.Parses("10.123e11").WithValue(10.123e11m);
+        JsonDocument.Parses("10.123E-11").WithValue(10.123E-11m);
     }
 
     public void ParsesQuotations()
@@ -39,8 +41,8 @@ class JsonGrammarTests : JsonGrammar
         var filled = "\"abc \\\" \\\\ \\/ \\b \\f \\n \\r \\t \\u263a def\"";
         const string expected = "abc \" \\ / \b \f \n \r \t ☺ def";
 
-        Json.Parses(empty).WithValue("");
-        Json.Parses(filled).WithValue(expected);
+        JsonDocument.Parses(empty).WithValue("");
+        JsonDocument.Parses(filled).WithValue(expected);
     }
 
     public void ParsesArrays()
@@ -48,9 +50,9 @@ class JsonGrammarTests : JsonGrammar
         var empty = "[]";
         var filled = "[0, 1, 2]";
 
-        Json.Parses(empty).WithValue(value => ((object[])value).ShouldBeEmpty());
+        JsonDocument.Parses(empty).WithValue(value => ((object[])value).ShouldBeEmpty());
 
-        Json.Parses(filled).WithValue(value => value.ShouldBe(new[] { 0m, 1m, 2m }));
+        JsonDocument.Parses(filled).WithValue(value => value.ShouldBe(new[] { 0m, 1m, 2m }));
     }
 
     public void ParsesDictionaries()
@@ -58,9 +60,9 @@ class JsonGrammarTests : JsonGrammar
         var empty = "{}";
         var filled = "{\"zero\" : 0, \"one\" : 1, \"two\" : 2}";
 
-        Json.Parses(empty).WithValue(value => ((Dictionary<string, object>)value).Count.ShouldBe(0));
+        JsonDocument.Parses(empty).WithValue(value => ((Dictionary<string, object>)value).Count.ShouldBe(0));
 
-        Json.Parses(filled).WithValue(value =>
+        JsonDocument.Parses(filled).WithValue(value =>
         {
             var dictionary = (Dictionary<string, object>) value;
             dictionary["zero"].ShouldBe(0m);
@@ -87,7 +89,7 @@ class JsonGrammarTests : JsonGrammar
 
             " + whitespaceCharacters;
 
-        Json.Parses(complex).WithValue(value =>
+        JsonDocument.Parses(complex).WithValue(value =>
         {
             var json = (Dictionary<string, object>)value;
             json["numbers"].ShouldBe(new[] {10m, 20m, 30m});
@@ -102,10 +104,10 @@ class JsonGrammarTests : JsonGrammar
 
     public void RequiresEndOfInputAfterFirstValidJsonValue()
     {
-        Json.FailsToParse("true $garbage$").LeavingUnparsedInput("$garbage$").WithMessage("(1, 6): end of input expected");
-        Json.FailsToParse("10.123E-11  $garbage$").LeavingUnparsedInput("$garbage$").WithMessage("(1, 13): end of input expected");
-        Json.FailsToParse("\"garbage\" $garbage$").LeavingUnparsedInput("$garbage$").WithMessage("(1, 11): end of input expected");
-        Json.FailsToParse("[0, 1, 2] $garbage$").LeavingUnparsedInput("$garbage$").WithMessage("(1, 11): end of input expected");
-        Json.FailsToParse("{\"zero\" : 0} $garbage$").LeavingUnparsedInput("$garbage$").WithMessage("(1, 14): end of input expected");
+        JsonDocument.FailsToParse("true $garbage$").LeavingUnparsedInput("$garbage$").WithMessage("(1, 6): end of input expected");
+        JsonDocument.FailsToParse("10.123E-11  $garbage$").LeavingUnparsedInput("$garbage$").WithMessage("(1, 13): end of input expected");
+        JsonDocument.FailsToParse("\"garbage\" $garbage$").LeavingUnparsedInput("$garbage$").WithMessage("(1, 11): end of input expected");
+        JsonDocument.FailsToParse("[0, 1, 2] $garbage$").LeavingUnparsedInput("$garbage$").WithMessage("(1, 11): end of input expected");
+        JsonDocument.FailsToParse("{\"zero\" : 0} $garbage$").LeavingUnparsedInput("$garbage$").WithMessage("(1, 14): end of input expected");
     }
 }
