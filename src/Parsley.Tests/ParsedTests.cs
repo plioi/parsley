15ac9116336ab803347f68060204e240a461a692
Patrick Lioi <patrick.lioi@gmail.com>
2022-04-14 13:23:08 -0500

    Phase out the use of Text instances as class fields, as a step towards changing Text from a `class` to a `ref struct`.

--- src/Parsley.Tests/ParsedTests.cs
+++ src/Parsley.Tests/ParsedTests.cs
@@ -2,21 +2,14 @@ namespace Parsley.Tests;
 
 class ParsedTests
 {
-    readonly Text unparsed;
-
-    public ParsedTests()
-    {
-        unparsed = new Text("0");
-    }
-
     public void HasAParsedValue()
     {
-        new Parsed<string>("parsed", unparsed.Position).Value.ShouldBe("parsed");
+        new Parsed<string>("parsed", new(1, 1)).Value.ShouldBe("parsed");
     }
 
     public void HasNoErrorMessageByDefault()
     {
-        new Parsed<string>("x", unparsed.Position).ErrorMessages.ShouldBe(ErrorMessageList.Empty);
+        new Parsed<string>("x", new(1, 1)).ErrorMessages.ShouldBe(ErrorMessageList.Empty);
     }
 
     public void CanIndicatePotentialErrors()
@@ -25,17 +18,17 @@ class ParsedTests
             .With(ErrorMessage.Expected("A"))
             .With(ErrorMessage.Expected("B"));
 
-        new Parsed<object>("x", unparsed.Position, potentialErrors).ErrorMessages.ShouldBe(potentialErrors);
+        new Parsed<object>("x", new(1, 1), potentialErrors).ErrorMessages.ShouldBe(potentialErrors);
     }
 
     public void HasRemainingUnparsedInput()
     {
-        var parsed = new Parsed<string>("parsed", unparsed.Position);
-        parsed.Position.ShouldBe(unparsed.Position);
+        var parsed = new Parsed<string>("parsed", new(12, 34));
+        parsed.Position.ShouldBe(new (12, 34));
     }
 
     public void ReportsNonerrorState()
     {
-        new Parsed<string>("parsed", unparsed.Position).Success.ShouldBeTrue();
+        new Parsed<string>("parsed", new(1, 1)).Success.ShouldBeTrue();
     }
 }
