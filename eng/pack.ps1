31a31eda9df58fd11b3f9a7b689de716a5a2c092
Patrick Lioi <patrick.lioi@gmail.com>
2023-11-14 16:42:08 -0600

    Rename the untracked root folder "artifacts" to "packages", for clarity of what it contains and to avoid mistaking it for the similarly named folder /src/packages which contains the bin/ and obj/ folders for each project of the solution.

--- eng/pack.ps1
+++ eng/pack.ps1
@@ -1,6 +1,6 @@
 . (Join-Path $PSScriptRoot utilities)
 
-if (test-path artifacts) { remove-item artifacts -Recurse }
+if (test-path packages) { remove-item packages -Recurse }
 
 exec { bash ./build }
-exec { dotnet pack src -c Release -o artifacts --no-build --nologo }
+exec { dotnet pack src -c Release -o packages --no-build --nologo }
