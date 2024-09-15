6dd08043efd56e06a54b88f0a8907afee1445674
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-14 20:08:12 -0500

    Drop the GitHub Report as it provides little value.

--- src/Parsley.Tests/TestProject.cs
+++ src/Parsley.Tests/TestProject.cs
@@ -6,7 +6,5 @@ class TestProject : ITestProject
 {
     public void Configure(TestConfiguration configuration, TestEnvironment environment)
     {
-        if (environment.IsContinuousIntegration())
-            configuration.Reports.Add(new GitHubReport(environment));
     }
 }
