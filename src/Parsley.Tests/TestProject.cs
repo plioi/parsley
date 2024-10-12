12f23566a75444339f9dc27996abe199481f944c
Patrick Lioi <patrick.lioi@gmail.com>
2024-10-11 21:31:14 -0500

    When a single assertion fails and the exception provides diff-ready expected/actual values, display them in a diff viewer.

--- src/Parsley.Tests/TestProject.cs
+++ src/Parsley.Tests/TestProject.cs
@@ -6,5 +6,7 @@ class TestProject : ITestProject
 {
     public void Configure(TestConfiguration configuration, TestEnvironment environment)
     {
+        if (environment.IsDevelopment())
+            configuration.Reports.Add<DiffToolReport>();
     }
 }
